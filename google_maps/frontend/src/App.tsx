89cc460ce52437871a40a18e8c03f6aad28aabfb
edgarlevyings44 <edgarlevy81@gmail.com>
2024-03-16 16:57:06 +0300

    add tailwind

--- google_maps/frontend/src/App.tsx
+++ google_maps/frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import MyComponent from "./map"
-function App() {
-  
+import TestComponent from "./test"
 
+
+function App() {
   return (
-    <>
+    <div className="bg-blue-500 h-screen">
       <MyComponent />
-    </>
+      <TestComponent />
+    </div>
   )
 }
 
